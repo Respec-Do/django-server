91ea760803ff40be9d3c1aa825258caa5f961d57
Respec-Do <kanghyundo@gmail.com>
2024-03-20 17:42:49 +0900

    탄력적 IP 설정

--- oneLabProject/settings.py
+++ oneLabProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m&1gjc)a!0m&t4-skkigdplzw%e&f)5j@ksztwce%#bqaksqs)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','3.35.219.200']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','3.38.231.216']
 
 
 # Application definition
