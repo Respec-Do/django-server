be17a333d57d855b3d5f918333312162e80a5079
Respec-Do <kanghyundo@gmail.com>
2024-03-21 10:20:07 +0900

    temp

--- notification/static/notification/js/list.js
+++ notification/static/notification/js/list.js
@@ -5,7 +5,7 @@ let page = 1
 const tabbtns = document.querySelectorAll("div.ui-tabs ul li");
 
 const getList = (callback) => {
-    fetch(`http://3.38.231.216/notification/list/${page}`)
+    fetch(`/notification/list/${page}`)
         .then((response) => response.json())
         .then((notifications) => {
             if(callback){
